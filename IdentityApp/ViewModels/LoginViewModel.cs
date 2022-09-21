70dd3274f4c32f2cb6ca63cf1681a74b77d834d5
Serhii Zakutko <serhii.zakutko@nixsolutions.com>
2022-09-21 11:33:33 +0200

    Login was added.

---
+++ IdentityApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Display(Name = "Forget?")]
+        public bool RememberMe { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
